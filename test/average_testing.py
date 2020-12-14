0b4da088fcaad0cd1f80e85e40d4207eb5d5e0b6
Quickinline <adelbouhraoua23@gmail.com>
2020-12-14 14:27:39 +0100

    updated the unit Test

--- test/average_testing.py
+++ test/average_testing.py
@@ -1,8 +1,8 @@
 import random
-def average(list):
+def average_correct(list):
     return sum(list)/len(list)
 
 def test_average():
     for x in range(5):
         list = [random.randint(5,200) for x in range(15)]
-        assert average(list) == average(list)
+        assert average_correct(list) == average(list)
